8ecbd66f98292d5d4b66bb2c61370f17d9bc10a2
Lukasz Motyl <motyl.lukasz@wp.pl>
2017-05-09 23:24:04 +0200

    #4 configured nginx with nodejs project

--- Vagrantfile
+++ Vagrantfile
@@ -14,7 +14,7 @@ Vagrant.configure("2") do |config|
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "ubuntu/xenial64"
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.network :forwarded_port, guest: 3000, host: 3000
+  # config.vm.network :forwarded_port, guest: 80, host: 80
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 NODE_VERSION=6.x
+USERNAME=ubuntu
 
 apt-get update
-apt-get install -y apache2
+apt-get install -y nginx
+# set up nginx server
+sudo cp /vagrant/.provision/nginx/nginx.conf /etc/nginx/sites-available/site.conf
+sudo cp -f /vagrant/.provision/etc/hosts /etc/hosts
+sudo chmod 644 /etc/nginx/sites-available/site.conf
+sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
+sudo service nginx restart
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
@@ -11,3 +18,7 @@ fi
 curl -sL https://deb.nodesource.com/setup_$NODE_VERSION | sudo -E bash -
 apt-get install -y nodejs
 apt-get install -y build-essential
+npm install -g pm2
+
+# at first starup you could need go to vagrant and write: pm2 start server.js
+env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $USERNAME --hp /home/$USERNAME
